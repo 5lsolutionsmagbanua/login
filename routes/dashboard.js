1d5b56a0f49b8baf4a67c632e80a5594e98b2668
5lsolutionsmagbanua@gmail.com <5lsolutionsmagbanua@gmail.com>
2026-04-24 17:01:14 +0800

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('dashboard', { title: 'Dashboard Page' });
+router.get('/', function (req, res, next) {
+    res.render('dashboard', { title: 'Express' });
 });
 
-module.exports = router;    
+
+router.get('/get-data/:name/:position', (req, res) => {
+    try {
+        const {name, position} = req.params;
+        console.log('Request: ', name, position);
+
+        res.status(200).json({ message: "Success", data: [{ name: name, position: position }] });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error })
+    }
+});
+
+module.exports = router;
