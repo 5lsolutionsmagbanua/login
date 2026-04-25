b6dbcb072aaac01e37c6c3cc10862a80429d5193
5lsolutionsmagbanua@gmail.com <5lsolutionsmagbanua@gmail.com>
2026-04-25 16:49:14 +0800

    update

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var dashboardRouter = require('./routes/dashboard');
 var ticketRouter = require('./routes/ticket');
+var createRouter = require('./routes/create');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views/layout'));
@@ -25,6 +26,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/ticket', ticketRouter);
+app.use('/create', createRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
