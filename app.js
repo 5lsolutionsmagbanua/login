88daa3b97bedf59c3a48fa470b7a7cbeb83685af
5lsolutionsmagbanua@gmail.com <5lsolutionsmagbanua@gmail.com>
2026-04-27 13:47:46 +0800

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var loginRouter = require('./routes/login');
 var dashboardRouter = require('./routes/dashboard');
 var ticketRouter = require('./routes/ticket');
 var createRouter = require('./routes/create');
+var slbuticketRouter = require('./routes/slbuticket');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views/layout'));
@@ -27,6 +28,7 @@ app.use('/login', loginRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/ticket', ticketRouter);
 app.use('/create', createRouter);
+app.use('/slbuticket', slbuticketRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -50,6 +52,7 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('login'); // renders register.ejs
 });
+
 
 
 module.exports = app;
---
+++ routes/slbuticket.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('slbuticket', { title: 'Tickets List SOUTH LUZON' });
+});
+
+router.get('/get-data/:name/:position', (req, res) => {
+    try {
+        const {name, position} = req.params;
+        console.log('Request: ', name, position);
+
+        res.status(200).json({ message: "Success", data: [{ name: name, position: position }] });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error })
+    }
+});
+
+module.exports = router;
